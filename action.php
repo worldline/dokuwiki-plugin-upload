f8231d727f2c8f6871a92ffa63642f269e6dd33a
lupo49 <dokuwiki@lupo49.de>
2014-01-02 12:52:06 +0100

    Initial commit

---
+++ action.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Upload Action Plugin:   Handle Upload and temporarily disabling cache of page.
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author    Franz Häfner <fhaefner@informatik.tu-cottbus.de>
+ * @author    Randolf Rotta <rrotta@informatik.tu-cottbus.de>
+ */
+ 
+if(!defined('DOKU_INC')) die();
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once DOKU_PLUGIN.'action.php';
+require_once(DOKU_INC.'inc/media.php');
+require_once(DOKU_INC.'inc/infoutils.php');
+
+class action_plugin_upload extends DokuWiki_Action_Plugin {
+
+    function getInfo(){
+        return array(
+            'author' => 'Franz Häfner',
+            'email'  => 'fhaefner@informatik.tu-cottbus.de',
+            'date'   => '2010-09-07',
+            'name'   => 'upload plugin',
+            'desc'   => 'upload plugin can add a link to the media manager in your wikipage.
+            			Basic syntax: {{upload>namespace|option1|option2}}
+				Use @page@ as namespage to use ID of the actual page as namespace or @current@ to use the namespace the current page is in.',
+            'url'    => 'http://wiki.splitbrain.org/plugin:upload',
+        );
+    }
+
+    /**
+     * Register its handlers with the DokuWiki's event controller
+     */
+    function register(&$controller) {
+        $controller->register_hook('PARSER_CACHE_USE', 'BEFORE', $this,
+                                   '_hook_function_cache');
+        $controller->register_hook('ACTION_HEADERS_SEND', 'BEFORE', $this,
+                                   '_hook_function_upload');
+    }
+ 
+ 
+	function _hook_function_cache(&$event, $param) {
+		if($_FILES['upload']['tmp_name']) {
+			$event->preventDefault();
+			$event->stopPropagation();
+			$event->result = false;
+		}
+
+		$namespace = p_get_metadata($event->data->page, 'has_upload_form');
+		if(!empty($namespace)) {
+			$event->data->key .= '|ACL' . auth_quickaclcheck($namespace);
+			$event->data->cache = getCacheName($event->data->key, $event->data->ext);
+		}
+	}
+
+    function _hook_function_upload(&$event, $param) {
+        // get namespace to display (either direct or from deletion order)
+		$NS = $_POST['ns'];
+		$NS = cleanID($NS);
+
+		// check auth
+		$AUTH = auth_quickaclcheck("$NS:*");
+		if($AUTH < AUTH_UPLOAD) {
+			msg($lang['uploadfail'], -1);
+			return;
+		}
+
+		// handle upload
+		if($_FILES['upload']['tmp_name']){
+			$_POST['id'] = $_POST['new_name'];
+			$JUMPTO = media_upload($NS,$AUTH);
+		    if($JUMPTO) {
+				$NS = getNS($JUMPTO);
+				$ID = $_POST['page'];
+				$NS = getNS($ID);
+			}
+		}
+    }
+}
---
+++ syntax.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Upload plugin, allows upload for users with correct 
+ * permission fromin a wikipage to a defined namespace.
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Christian Moll <christian@chrmoll.de>
+ * @author    Franz Häfner <fhaefner@informatik.tu-cottbus.de>
+ * @author    Randolf Rotta <rrotta@informatik.tu-cottbus.de>
+ */
+
+if(!defined('NL')) define('NL',"\n");
+if(!defined('DOKU_INC')) define('DOKU_INC',dirname(__FILE__).'/../../');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
+require_once(DOKU_INC.'inc/media.php');
+require_once(DOKU_INC.'inc/auth.php');
+
+require_once(DOKU_INC.'inc/infoutils.php');
+
+class syntax_plugin_upload extends DokuWiki_Syntax_Plugin {
+
+	/**
+	 * Print the media upload form if permissions are correct
+	 * 
+	 * @author Christian Moll <christian@chrmoll.de>
+	 * @author Andreas Gohr <andi@splitbrain.org>
+	 * @author    Franz Häfner <fhaefner@informatik.tu-cottbus.de>
+     * @author    Randolf Rotta <rrotta@informatik.tu-cottbus.de>
+	 */
+	function upload_plugin_uploadform($ns, $auth, $options){
+		global $ID;
+		global $lang;
+
+		if($auth < AUTH_UPLOAD) return;
+		
+		$params = array();
+		$params['id'] = 'upload_plugin';
+		$params['action'] = DOKU_BASE.'doku.php?id='.$ID;
+		$params['method'] = 'post';
+		$params['enctype'] = 'multipart/form-data';
+		$params['class'] = 'upload__plugin';
+		
+		// Modification of the default dw HTML upload form
+		$form = new Doku_Form($params);
+		$form->startFieldset($lang['fileupload']);
+		$form->addElement(formSecurityToken());
+		$form->addHidden('page', hsc($ID));
+		$form->addHidden('ns', hsc($ns));
+		$form->addElement(form_makeFileField('upload', $lang['txt_upload'].':', 'upload__file'));
+		if($options['renameable']) {
+		          // don't name this field here "id" because it is misinterpreted by DokuWiki if the upload form is not in media manager
+		          $form->addElement(form_makeTextField('new_name', '', $lang['txt_filename'].':', 'upload__name'));
+		}
+		
+		if($auth >= AUTH_DELETE){
+		   if($options['overwrite']) {
+		     //$form->addElement(form_makeCheckboxField('ow', 1, $lang['txt_overwrt'], 'dw__ow', 'check'));
+		     // circumvent wrong formatting in doku_form
+		     $form->addElement('<label class="check" for="dw__ow">'.
+				       '<span>'.$lang['txt_overwrt'].'</span>'.
+				       '<input type="checkbox" id="dw__ow" name="ow" value="1"/>'.
+				       '</label>');
+		   }
+		}
+		$form->addElement(form_makeButton('submit', '', $lang['btn_upload']));
+		$form->endFieldset();
+		
+		$html .= '<div class="upload_plugin"><p>'.NL;
+		$html .= $form->getForm();
+		$html .= '</p></div>'.NL;
+		return $html;
+	}
+ 
+    function getInfo(){
+        return array(
+            'author' => 'Franz Häfner',
+            'email'  => 'fhaefner@informatik.tu-cottbus.de',
+            'date'   => '2010-09-07',
+            'name'   => 'upload plugin',
+            'desc'   => 'upload plugin can add a link to the media manager in your wikipage.
+            			Basic syntax: {{upload>namespace|option1|option2}}
+				Use @page@ as namespage to use ID of the actual page as namespace or @current@ to use the namespace the current page is in.',
+            'url'    => 'http://wiki.splitbrain.org/plugin:upload',
+        );
+    }
+ 
+    function getType(){
+        return 'substition';
+    }
+    
+    function getSort(){
+        return 32;
+    }
+ 
+    function connectTo($mode) {
+      $this->Lexer->addSpecialPattern('\{\{upload>.+?\}\}',$mode,'plugin_upload');
+    }
+	
+    function handle($match, $state, $pos, &$handler){
+   
+		global $ID;
+   
+		$match = substr($match,9, -2);
+		$matches = explode('|', $match, 2);
+		$o = explode('|', $matches[1]);
+		
+		$options['overwrite'] = in_array('OVERWRITE', $o);
+		$options['renameable'] = in_array('RENAMEABLE', $o);
+		
+		$ns=$matches[0];
+		
+		if($ns=='@page@') $ns=$ID;
+		else if($ns=='@current@') $ns=getNS($ID);
+		else resolve_pageid(getNS($ID), $ns, $exists);
+		
+		return array('uploadns'=>hsc($ns),'para'=>$options);
+    }
+ 
+    function render($mode, &$renderer, $data){
+		if($mode == 'xhtml'){
+			
+			//check auth
+			$auth = auth_quickaclcheck($data['uploadns'].':*');
+			
+			if($auth >= AUTH_UPLOAD) {
+				$renderer->doc.=$this->upload_plugin_uploadform($data['uploadns'], $auth, $data['para']);
+//				$renderer->info['cache'] = false;
+			}
+			return true;
+		} else if($mode == 'metadata') {
+			$renderer->meta['has_upload_form'] = $data['uploadns'].':*';
+			return true;
+		}
+		return false;
+    }
+
+}
+ 
+//Setup VIM: ex: et ts=4 enc=utf-8 :
